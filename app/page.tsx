fd6d28f3b9fb97c91b7a6d45ef19badf8c65e05f
Nataliya Kim <magistr1982@gmail.com>
2024-11-12 15:22:50 +0200

    Add variables to the global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl">StoreIT - The only storage solution you need.</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">StoreIT - The only storage solution you need.</h1>
     </div>
   );
 };
