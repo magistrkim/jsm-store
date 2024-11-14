27b9734190564981040b3b29d88ad31543da81c7
Nataliya Kim <magistr1982@gmail.com>
2024-11-14 17:33:14 +0200

    Add shadcn to the project

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+type FormType = "sign-in" | "sign-up";
 
-const AuthForm = () => {
+const AuthForm = ({ type }: { type: FormType }) => {
   return <div className="text-green">AuthForm</div>;
 };
 
