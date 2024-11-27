90db9aa92e22fd204aaed3764476b1c54b32849a
Nataliya Kim <magistr1982@gmail.com>
2024-11-27 10:38:30 +0200

    Add createAccount function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { ID, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 
@@ -7,8 +8,18 @@ const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
   const result = await databases.listDocuments(
     appwriteConfig.databaseId,
-    appwriteConfig.usersCollectionId
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
   );
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+  try {
+      const session = await account.createEmailToken(ID.unique(), email);
+      return session.userId
+  } catch (error) {}
 };
 
 const createAccount = async ({
@@ -19,4 +30,5 @@ const createAccount = async ({
   email: string;
 }) => {
   const existingUser = await getUserByEmail(email);
+  const accountId = await sendEmailOTP({ email });
 };
