53c5b61d12078e0c482d4e8b8d89230fec147823
Nataliya Kim <magistr1982@gmail.com>
2024-11-27 10:31:18 +0200

    Create getUserByEmail function

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId
+  );
+};
+
+const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+};
