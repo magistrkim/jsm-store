14edfca82b2d0066a7b071841d9f82154be9e555
Nataliya Kim <magistr1982@gmail.com>
2024-12-05 16:05:12 +0200

    Create Search component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import FileUploader from "./FileUploader";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import Search from "./Search";
 
 const Header = () => {
   return (
     <header className="header">
-      Search
+      <Search />
       <div className="header-wrapper">
         <FileUploader />
+        <form action="">
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
       </div>
     </header>
   );
