3163254e9a5d4b5b69b6033a80e55c1a9b48d660
Nataliya Kim <magistr1982@gmail.com>
2024-12-05 16:17:05 +0200

    Add sidebar UI elements

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>SIDEBAR</div>
-  )
-}
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full-brand.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="hidden h-auto lg:block"
+        />
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+      </Link>
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+          <li></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
