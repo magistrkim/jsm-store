e94c23c62f3921469e72767eda1e2024da7f7e27
Nataliya Kim <magistr1982@gmail.com>
2024-12-05 15:53:44 +0200

    Create MobileNav component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,15 @@
 import Header from "@/components/Header";
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex h-screen">
-      <Sidebar/>
+      <Sidebar />
       <section className="flex h-full flex-1 flex-col ">
-        MobileNavigation 
-        <Header/>
+        <MobileNav />
+        <Header />
         <div className="main-content">{children}</div>
       </section>
     </main>
