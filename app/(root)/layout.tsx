61c0fdce96cc221ba0487ee6bfdfa9ee17510cda
Nataliya Kim <magistr1982@gmail.com>
2024-12-05 15:51:36 +0200

    Create Sidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex h-screen">
-      Sidebar
+      <Sidebar/>
       <section className="flex h-full flex-1 flex-col ">
         MobileNavigation Header
         <div className="main-content">{children}</div>
