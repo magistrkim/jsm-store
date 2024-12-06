a661b1474eb84d23ce9a4bfc87968ebd3534de40
Nataliya Kim <magistr1982@gmail.com>
2024-12-06 16:20:21 +0200

    Complete Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,16 @@
 import Header from "@/components/Header";
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
-  
+const layout = async({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) return redirect("/sign-in");
   return (
     <main className="flex h-screen">
-      <Sidebar />
+      <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col ">
         <MobileNav />
         <Header />
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,17 @@
 "use client";
-import { avatarPlaceholderUrl, navItems } from "@/constants";
+import { navItems } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-const Sidebar = () => {
+interface Props {
+  fullName: string;
+  avatar: string;
+  email: string;
+}
+const Sidebar = ({ fullName, avatar, email }: Props) => {
   const pathname = usePathname();
   return (
     <aside className="sidebar">
@@ -61,15 +66,15 @@ const Sidebar = () => {
       />
       <div className="sidebar-user-info">
         <Image
-          src={avatarPlaceholderUrl}
+          src={avatar}
           alt="avatar"
           width={44}
           height={44}
           className="sidebar-user-avatar"
         />
         <div className="hidden lg:block">
-          {/* <p className="subtitle-2 capitalize">{fullName}</p>
-          <p className="caption">{email}</p> */}
+          <p className="subtitle-2 capitalize">{fullName}</p>
+          <p className="caption">{email}</p>
         </div>
       </div>
     </aside>
